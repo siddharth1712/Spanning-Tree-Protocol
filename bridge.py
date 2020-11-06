dfbf3efd6a0064e81cde8632d93e64b9061a6b5b
Vineet Gala <55905703+vineetgala@users.noreply.github.com>
2020-11-06 23:55:31 +0530

    Identifies Root Port

--- bridge.py
+++ bridge.py
@@ -1,14 +1,27 @@
 class bridge:
     def __init__(self):
+        self.root=-1
+        self.rp=['\0',-1]
+        self.rootd=-1
         self.id=-1
         self.ftable={}
-        self.conn_lans={}   #second entry is 1 for RP, 0 for DP, -1 for NP
+        self.conn_lans=[]   #second entry is 1 for RP, 0 for DP, -1 for NP
+    def disp(self):
+        print(self.root,self.rp,self.rootd,self.id,self.conn_lans)
 class lan:
     def __init__(self):
+        self.dp=-1
         self.id='\0'
-        self.conn_br={}  #second entry is 1 for RP, 0 for DP, -1 for NP
+        self.conn_br=[]  #second entry is 1 for RP, 0 for DP, -1 for NP
         self.hosts=[]
-        
+class message:
+    def __init__(self):
+        self.port='\0'
+        self.stop=False
+        self.src_id=-1
+        self.dest_id=-1
+        self.root=-1
+        self.d=-1
 #-------------------------------------------------
 def take_input(tr,n,br_list,lan_list):
     tr=int(input())
@@ -22,16 +35,19 @@ def take_input(tr,n,br_list,lan_list):
         temp_bridge_obj=bridge()
         br_id=int(inp[0][1:-1])
         temp_bridge_obj.id=br_id
+        temp_bridge_obj.root=br_id
         
         for temp_l in inp[1:]:  #2
-            temp_bridge_obj.conn_lans.update({temp_l:0}) #initialising all connections to DP
+            temp_bridge_obj.conn_lans.append(temp_l) #initialising all connections to DP
+            temp_bridge_obj.conn_lans=list(set(temp_bridge_obj.conn_lans))
             
             if temp_l not in lan_list:
                 temp_lan_obj=lan()
                 temp_lan_obj.id=temp_l
                 lan_list.update({temp_l:temp_lan_obj})
 
-            lan_list[temp_l].conn_br.update({br_id:0})
+            lan_list[temp_l].conn_br.append(br_id)
+            lan_list[temp_l].conn_br=list(set(lan_list[temp_l].conn_br))
 
         br_list.update({br_id:temp_bridge_obj})
         
@@ -44,7 +60,7 @@ def take_input(tr,n,br_list,lan_list):
 
     ''''''
     for bridge_id,brobj in br_list.items():
-        print(brobj.id,brobj.conn_lans)
+        brobj.disp()
     for lan_id,lanobj in lan_list.items():
         print(lanobj.id,lanobj.conn_br,lanobj.hosts)
     ''''''
@@ -53,3 +69,77 @@ def take_input(tr,n,br_list,lan_list):
     
     return tr,n,br_list,lan_list
 
+def send(m,br_list,lan_list):
+    #print(m.d,m.src_id,m.root)
+    received=[]
+    for lan_id in br_list[m.src_id].conn_lans:
+        for br_id in lan_list[lan_id].conn_br:
+            if(br_id!=m.src_id):
+                received.append(message())
+                received[-1].root=m.root
+                received[-1].d=m.d
+                received[-1].src_id=m.src_id
+                received[-1].dest_id=br_id
+                received[-1].port=lan_id
+    return received                    
+
+def fwd(m,br_list,lan_list):
+    m1=message()
+    m1.root=m.root
+    m1.d=m.d+1
+    m1.src_id=m.dest_id
+    c1=  (m.root < br_list[m1.src_id].root)
+    c2= (m.root == br_list[m1.src_id].root) & (br_list[m1.src_id].rootd > m.d+1)
+    c3= (m.root == br_list[m1.src_id].root) & (br_list[m1.src_id].rootd == m.d+1) & (br_list[m1.src_id].rp[1] > m.src_id)
+    if c1|c2|c3:
+        br_list[m1.src_id].root=m.root
+        br_list[m1.src_id].rp=[m.port,m.src_id]
+        br_list[m1.src_id].rootd=m.d+1
+    else:
+        m1.stop=True
+    return m1,br_list
+
+def initialize(tr,n,br_list,lan_list):
+    sent=[]
+    recd=[]
+    for bridge_id,br_obj in br_list.items():
+        m=message()
+        m.d=0
+        m.src_id=br_obj.id
+        m.root=br_obj.id
+        sent.append(m)
+        print(m.d,m.src_id,m.root)
+    first=True
+    while len(sent)>0:
+        if not first:
+            sent.clear()
+        for m in recd:
+            print(m.d,m.src_id,m.dest_id,m.root,m.port,m.stop)
+            temp,br_list=fwd(m,br_list,lan_list)
+            if not temp.stop:
+                sent.append(temp)
+        recd.clear()
+        print("============")
+        for m in sent:
+            #print(m.d,m.src_id,m.root)
+            print(m.d,m.src_id,m.dest_id,m.root,m.port,m.stop)
+            recd=recd+(send(m,br_list,lan_list))
+        print("------------")
+        new_recd=[]
+        for i in recd:
+            matched=False
+            for elem in new_recd:
+                if (i.port==elem.port) & (i.stop==elem.stop) & (i.src_id==elem.src_id) & (i.dest_id==elem.dest_id) & (i.root==elem.root) & (i.d==elem.d):
+                    matched=True
+                    break
+            if not matched:
+                new_recd.append(i)
+        recd=new_recd
+        first=False
+
+    for temp,b in br_list.items():
+        b.disp()
+    
+    return br_list,lan_list
+
+    
--- bridgesim.py
+++ bridgesim.py
@@ -5,3 +5,4 @@ n=0
 br_list={}
 lan_list={}
 tr,n,br_list,lan_list=bridge.take_input(tr,n,br_list,lan_list)
+br_list,lan_list=bridge.initialize(tr,n,br_list,lan_list)
